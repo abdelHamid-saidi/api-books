097c742d3500075972019611ac2cce5c509b6f85
Abdelhamid Saidi <saidiabdelhamid.office@gmail.com>
2026-05-19 12:08:05 +0200

    activite 1, 2, 3

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ Route::get('/ping', function () {
 });
 
 Route::apiResource('books', BookController::class);
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{book}', [BookController::class, 'update']);
+    Route::patch('/books/{book}', [BookController::class, 'update']);
+    Route::delete('/books/{book}', [BookController::class, 'destroy']);
+});
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
